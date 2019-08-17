ead36f33f66d9006f38cde0e7cc148ff70350725
Yang Liu <yang.liu@mdpi.com>
2019-08-17 11:28:18 +0800

    1

--- zan.php
+++ zan.php
@@ -4,6 +4,9 @@ $ip =getip(); //获取用户IP
 $id = $_POST['id'];
 if(!isset($id) || empty($id)) exit;
 
+$check = $_POST['check'];
+
+
 //查询已赞过的IP
 $dsql->SetQuery("SELECT ip FROM dede_zan WHERE aid='".$id."' and ip='$ip'");
 $dsql->Execute();
@@ -15,9 +18,14 @@ $dsql->ExecuteNoneQuery("update dede_archives set zan=zan+1 where id='$id'; ");/
 
 $dsql->ExecuteNoneQuery("insert into dede_zan (aid,ip) values ('$id','$ip'); ");//写入IP,及被赞的AID
 
-$rows = $dsql->GetOne("Select zan from dede_archives where id='".$id."'");//获取被赞的数量
-$zan = $rows['zan']; //获取赞数值
-echo $zan;
+
 }else{
-echo "赞过了..";
+    //echo "赞过了..";
+    $dsql->ExecuteNoneQuery("update dede_archives set zan=zan-1 where id='$id'; ");//写入赞数
+    $dsql->ExecuteNoneQuery("delete from dede_zan where ip='$ip' and aid='$id' ");//删除IP,及被赞的AID
+
 }
+
+$rows = $dsql->GetOne("Select zan from dede_archives where id='".$id."'");//获取被赞的数量
+$zan = $rows['zan'];//获取赞数值
+echo $zan;
